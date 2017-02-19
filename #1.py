2114410d11690ee52dfe087cbda69cb3857ed6fe
Farmaan-PC <devfarmaan@gmail.com>
2017-02-19 23:46:54 +0530

    feat:Added Problem #2
    Problem Description:Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

--- #1.py
+++ #1.py
@@ -1,9 +1,14 @@
 from libs.math import natural_number_sum
 
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 def main():
-    T = int(input().rstrip())
-    for _ in range(T):
+    t = int(input().rstrip())
+    for _ in range(t):
         N = int(input().rstrip())
         print(3 * natural_number_sum((N - 1) // 3) +
               5 * natural_number_sum((N - 1) // 5) - 15 *
--- libs/math.py
+++ libs/math.py
@@ -1,2 +1,11 @@
 def natural_number_sum(number):
-    return number*(number+1)//2
+    return number * (number + 1) // 2
+
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    while a + b < n:
+        a, b = b, a + b
+        if b % 2 == 0:
+            yield b
